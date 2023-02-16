00724a5cb6583eb3dadb11cae883afeb89e85c3c
Alessio Piccioli <piccioli@netseven.it>
2023-02-16 18:34:49 +0100

    Added proper role to admin user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -19,6 +20,7 @@ class UserSeeder extends Seeder
             'name' => 'Webmapp',
             'email' => 'team@webmapp.it',
             'password' => bcrypt('webmapp'),
+            'roles' => [UserRole::Admin]
         ])->markEmailAsVerified();
 
         User::factory(100)->create();
