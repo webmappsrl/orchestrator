c3633d827fd23ad526bf62492e880917a648c913
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-02-16 14:43:37 +0100

    Footer and Logo for Orchestrator dashboard. team@webmapp user added

---
+++ resources/views/nova/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="text-center">
+    <p>ORCHESTRATOR</p>
+    <p>Copyright <a class="font-bold text-green-600" target="blank" href="https://webmapp.it/">WEBMAPP</a>
+    </p>
+    <span class="px-1">&middot;</span>
+    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    v{{ \Laravel\Nova\Nova::version() }}
+    <span class="px-1">&middot;</span>
+    <a href="https://laravel.com/" class="text-primary dim no-underline">Laravel</a>
+    v{{ app()->version() }}
+    <span class="px-1">&middot;</span>
+    <a href="https://php.net/" class="text-primary dim no-underline">PHP</a>
+    v{{ phpversion() }}
+</div>
