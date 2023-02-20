7f0ad54f63ce1e27739deb82909c0c0de95bb18c
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-02-20 12:54:53 +0100

    Only admin can see users. Status set to select menu with default value to 'new', in Story field.

--- app/Nova/Story.php
+++ app/Nova/Story.php
@@ -8,6 +8,7 @@ use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Story extends Resource
@@ -47,7 +48,13 @@ class Story extends Resource
             ID::make()->sortable(),
             Text::make(__('Name'), 'name')->sortable(),
             Text::make(__('Description'), 'description')->hideFromIndex(),
-            Text::make(__('Status'), 'status'),
+            Select::make('Status')->options([
+                'new' => 'New',
+                'progress' => 'Progress',
+                'test' => 'Test',
+                'rejected' => 'Rejected',
+                'done' => 'Done',
+            ])->default('new')->displayUsingLabels(),
             BelongsTo::make('User'),
             BelongsTo::make('Epic')
         ];
