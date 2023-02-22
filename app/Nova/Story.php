4c942476a2950198b9f414232ce2c94585131bc7
Alessio Piccioli <piccioli@netseven.it>
2023-02-22 15:14:06 +0100

    Check on epic_id

--- app/Nova/Story.php
+++ app/Nova/Story.php
@@ -54,9 +54,8 @@ class Story extends Resource
             Select::make('Status')->options(collect(StoryStatus::cases())->pluck('name', 'value'))->default(StoryStatus::New->value)->displayUsingLabels(),
             Textarea::make(__('Description'), 'description')->hideFromIndex(),
             BelongsTo::make('User')->default(function ($request) {
-                $epicID = $request->input('viaResourceId');
-                $epic = Epic::find($epicID);
-                return $epic->user_id;
+                $epic = Epic::find($request->input('viaResourceId'));
+                return $epic ? $epic->user_id : null;
             }),
             BelongsTo::make('Epic'),
         ];
