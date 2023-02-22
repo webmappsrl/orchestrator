5dd225518fb30f1b00b08cdcb065980c20386668
RubensGarofalo <rubensgiuseppegarofalo@gmail.com>
2023-02-22 15:36:30 +0100

    Menu customization and App policy updated

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,9 +2,21 @@
 
 namespace App\Providers;
 
+use App\Nova\App;
+use App\Nova\Customer;
+use App\Nova\Epic;
+use App\Nova\Layer;
+use App\Nova\Milestone;
+use App\Nova\Project;
+use App\Nova\User;
+use Illuminate\Http\Request;
 use Laravel\Nova\Nova;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Blade;
+use Laravel\Nova\Dashboards\Main;
+use Laravel\Nova\Menu\Menu;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
@@ -17,6 +29,31 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        Nova::mainMenu(function (Request $request) {
+            return [
+                MenuSection::dashboard(Main::class)->icon('chart-bar'),
+
+                MenuSection::make('ADMIN', [
+                    MenuItem::resource(User::class),
+                ])->icon('user')->collapsable(),
+
+                MenuSection::make('APP', [
+                    MenuItem::resource(App::class),
+                    MenuItem::resource(Layer::class),
+                ])->icon('document-text')->collapsable(),
+
+                MenuSection::make('CRM', [
+                    MenuItem::resource(Customer::class),
+                    MenuItem::resource(Project::class),
+                ])->icon('users')->collapsable(),
+
+                MenuSection::make('DEV', [
+                    MenuItem::resource(Milestone::class),
+                    MenuItem::resource(Epic::class),
+                ])->icon('code')->collapsable(),
+            ];
+        });
 
         $this->getFooter();
     }
