a6657d93606905296053d770c463f8cfc6b9fe57
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-02-23 11:29:16 +0100

    Implemented function that get the epic status based on related stories status. Now epic status automatically update whenever a story is created/updated.  Wrote test for getStatusFromStories() function

--- app/Models/Epic.php
+++ app/Models/Epic.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\EpicStatus;
+use App\Enums\StoryStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,7 +22,7 @@ class Epic extends Model
     {
         return $this->hasMany(Story::class);
     }
-    
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -29,5 +31,41 @@ class Epic extends Model
     public function milestone()
     {
         return $this->belongsTo(Milestone::class);
+    }
+
+    /**
+     * Check epic status based on stories status
+     * Se la epica non ha storie -> status=new,
+     * quando la epica ha tutte le storie in status new ha status new, 
+     * quando la epica ha tutte le storie in status test è in status test, 
+     * quando la epica ha tutte le storie in status done ha status done.
+     * quando la epica ha almeno una storia con status diverso da new ha stato in progress, 
+     *
+     * @return EpicStatus
+     */
+    public function getStatusFromStories(): EpicStatus
+    {
+        $totalStories = $this->stories();
+        $newStories = $this->stories()->where('status', StoryStatus::New)->get();
+        $TestStories = $this->stories()->where('status', StoryStatus::Test)->get();
+        $doneStories = $this->stories()->where('status', StoryStatus::Done)->get();
+
+        if ($totalStories->count() == 0) {
+            return EpicStatus::New;
+        }
+
+        if ($newStories->count() == $totalStories->count()) {
+            return EpicStatus::New;
+        }
+
+        if ($TestStories->count() == $totalStories->count()) {
+            return EpicStatus::Test;
+        }
+
+        if ($doneStories->count() == $totalStories->count()) {
+            return EpicStatus::Done;
+        }
+
+        return EpicStatus::Progress;
     }
 }
