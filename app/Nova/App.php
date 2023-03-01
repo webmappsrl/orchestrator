4f7f20852eb2ec118d0e46ae3c43e96928047cf3
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-01 12:28:00 +0100

    Refactoring App Tab

--- app/Nova/App.php
+++ app/Nova/App.php
@@ -2,12 +2,16 @@
 
 namespace App\Nova;
 
+use App\Enums\UserRole;
 use Eminiarts\Tabs\Tabs;
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Fields\MultiSelect;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Kongulov\NovaTabTranslatable\NovaTabTranslatable;
 
@@ -66,16 +70,23 @@ class App extends Resource
             HasMany::make('Layers'), //display the relation with layers in nova field
             Text::make('API type', 'api')->sortable()->onlyOnDetail(),
             Text::make('Customer Name'),
+        ];
+    }
 
-            Tabs::make('App', [
-                'General' => [
-                    Text::make('App ID')->sortable(),
-                    Text::make(__('APP'), function () {
-                        $urlAny = 'https://' . $this->model()->id . '.app.webmapp.it';
-                        $urlDesktop = 'https://' . $this->model()->id . '.app.geohub.webmapp.it';
-                        $urlMobile = 'https://' . $this->model()->id . '.mobile.webmapp.it';
-                        return "
-               <a style='display: inline-flex;
+    public function fieldsForIndex()
+    {
+
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Text::make('API type', 'api')->sortable(),
+            Text::make('Name')->sortable(),
+            Text::make('Customer Name'),
+            Text::make(__('APP'), function () {
+                $urlAny = 'https://' . $this->model()->id . '.app.webmapp.it';
+                $urlDesktop = 'https://' . $this->model()->id . '.app.geohub.webmapp.it';
+                $urlMobile = 'https://' . $this->model()->id . '.mobile.webmapp.it';
+                return "
+                        <a style='display: inline-flex;
                         align-items: center;
                         justify-content: center;
                         padding-left: 0.75rem;
@@ -92,7 +103,7 @@ class App extends Resource
                         onmouseout='this.style.backgroundColor = \"#79c35b\";'
                         href='$urlAny' target='_blank'>ANY</a>
 
-                <a style='display: inline-flex;
+                        <a style='display: inline-flex;
                         align-items: center;
                         justify-content: center;
                         padding-left: 0.75rem;
@@ -108,8 +119,8 @@ class App extends Resource
                         onmouseover='this.style.backgroundColor = \"#5a9c3f\";'
                         onmouseout='this.style.backgroundColor = \"#79c35b\";'
                         href='$urlDesktop' target='_blank'>DESKTOP</a>
-                        
-                <a style='display: inline-flex;
+
+                        <a style='display: inline-flex;
                         align-items: center;
                         justify-content: center;
                         padding-left: 0.75rem;
@@ -125,13 +136,50 @@ class App extends Resource
                         onmouseover='this.style.backgroundColor = \"#5a9c3f\";'
                         onmouseout='this.style.backgroundColor = \"#79c35b\";'
                         href='$urlMobile' target='_blank'>MOBILE</a>";
-                    })->asHtml(),
-
-
-                ]
-            ])
+            })->asHtml()
         ];
     }
+
+    public function fieldsForDetail(Request $request)
+    {
+
+        if ($request->user()->hasRole(UserRole::Admin) || $request->user()->hasRole(UserRole::Editor)) {
+            return [
+                (new Tabs("APP Details: {$this->name} ({$this->id})", $this->sections()))->withToolbar(),
+            ];
+        }
+    }
+
+    public function fieldsForCreate()
+    {
+        $availableLanguages = is_null($this->model()->available_languages) ? [] : json_decode($this->model()->available_languages, true);
+
+        return [
+            Select::make(__('Api API'), 'api')->options(
+                [
+                    'elbrus' => 'Elbrus',
+                    'webmapp' => 'WebMapp',
+                    'webapp' => 'WebApp',
+                ]
+            )->required(),
+            Text::make(__('App Id'), 'app_id')->required(),
+            Text::make(__('Name'), 'name')->sortable()->required(),
+            Text::make(__('Customer Name'), 'customer_name')->sortable()->required(),
+            Select::make(__('Default Language'), 'default_language')->hideFromIndex()->options($this->languages)->displayUsingLabels()->required(),
+            //todo: fix multiselect (array to string conversion error)
+            Multiselect::make(__('Available Languages'), 'available_languages')->hideFromIndex()->options($this->languages, $availableLanguages)
+        ];
+    }
+
+    public function fieldsForUpdate(Request $request)
+    {
+        if ($request->user()->hasRole(UserRole::Admin) || $request->user()->hasRole(UserRole::Editor)) {
+            return [
+                (new Tabs("APP Details: {$this->name} ({$this->id})", $this->sections()))->withToolbar(),
+            ];
+        }
+    }
+
 
     /**
      * Get the cards available for the request.
@@ -175,5 +223,104 @@ class App extends Resource
     public function actions(NovaRequest $request)
     {
         return [];
+    }
+
+    public function sections()
+    {
+        return [
+            'APP' => $this->app_tab(),
+            // 'WEBAPP' => $this->webapp_tab(),
+            // 'HOME' => $this->home_tab(),
+            // 'PROJECT' => $this->project_tab(),
+            // 'AUTH' => $this->auth_tab(),
+            // 'OFFLINE' => $this->offline_tab(),
+            // 'ICONS' => $this->icons_tab(),
+            // 'LANGUAGES' => $this->languages_tab(),
+            // 'MAP' => $this->map_tab(),
+            // 'OPTIONS' => $this->options_tab(),
+            // 'ROUTING' => $this->routing_tab(),
+            // 'TABLE' => $this->table_tab(),
+            // 'THEME' => $this->theme_tab(),
+
+        ];
+    }
+
+
+    protected function app_tab(): array
+    {
+        return [
+            Select::make(__('API type'), 'api')->options(
+                [
+                    'elbrus' => 'Elbrus',
+                    'webmapp' => 'WebMapp',
+                    'webapp' => 'WebApp',
+                ]
+            )->required(),
+            Text::make(__('App Id'), 'app_id')->required(),
+            Text::make(__('Name'), 'name')->sortable()->required(),
+            Text::make(__('Customer Name'), 'customer_name')->sortable()->required(),
+            Text::make(__('Play Store link (android)'), 'android_store_link'),
+            Text::make(__('App Store link (iOS)'), 'ios_store_link'),
+            Text::make(__('APP'), function () {
+                $urlAny = 'https://' . $this->model()->id . '.app.webmapp.it';
+                $urlDesktop = 'https://' . $this->model()->id . '.app.geohub.webmapp.it';
+                $urlMobile = 'https://' . $this->model()->id . '.mobile.webmapp.it';
+                return "
+                        <a style='display: inline-flex;
+                        align-items: center;
+                        justify-content: center;
+                        padding-left: 0.75rem;
+                        padding-right: 0.75rem;
+                        margin: 2px;
+                        font-size: 1rem;
+                        line-height: 1.5;
+                        text-align: center;
+                        cursor: pointer;
+                        background-color: #79c35b;
+                        color: #fff;
+                        border-radius: 0.25rem;'
+                        onmouseover='this.style.backgroundColor = \"#5a9c3f\";'
+                        onmouseout='this.style.backgroundColor = \"#79c35b\";'
+                        href='$urlAny' target='_blank'>ANY</a>
+
+                        <a style='display: inline-flex;
+                        align-items: center;
+                        justify-content: center;
+                        padding-left: 0.75rem;
+                        padding-right: 0.75rem;
+                        margin: 2px;
+                        font-size: 1rem;
+                        line-height: 1.5;
+                        text-align: center;
+                        cursor: pointer;
+                        background-color: #79c35b;
+                        color: #fff;
+                        border-radius: 0.25rem;'
+                        onmouseover='this.style.backgroundColor = \"#5a9c3f\";'
+                        onmouseout='this.style.backgroundColor = \"#79c35b\";'
+                        href='$urlDesktop' target='_blank'>DESKTOP</a>
+
+                        <a style='display: inline-flex;
+                        align-items: center;
+                        justify-content: center;
+                        padding-left: 0.75rem;
+                        padding-right: 0.75rem;
+                        margin: 2px;
+                        font-size: 1rem;
+                        line-height: 1.5;
+                        text-align: center;
+                        cursor: pointer;
+                        background-color: #79c35b;
+                        color: #fff;
+                        border-radius: 0.25rem;'
+                        onmouseover='this.style.backgroundColor = \"#5a9c3f\";'
+                        onmouseout='this.style.backgroundColor = \"#79c35b\";'
+                        href='$urlMobile' target='_blank'>MOBILE</a>";
+            })->asHtml()->onlyOnDetail(),
+            Textarea::make('social_track_text')
+                ->help(__('Add a description for meta tags of social share. You can customize the description with these keywords: {app.name} e {track.name}'))
+                ->placeholder('Add social Meta Tag for description'),
+            Boolean::make('dashboard_show'),
+        ];
     }
 }
