dd4a922b6b48584dee5fa940904eac2a3606227a
Pedram Katanchi <pedramkat@gmail.com>
2023-03-15 15:10:55 +0100

    update deploy script

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -7,26 +7,15 @@ echo "Deployment started ..."
 # if already is in maintenance mode
 (php artisan down) || true
 
-# Pull the latest version of the app
-git pull origin develop
 
 # Install composer dependencies
 composer install  --no-interaction --prefer-dist --optimize-autoloader
 # php artisan nova:install
 
-# Run database migrations
-php artisan migrate:fresh --force --seed
-
-# Clear caches
-php artisan cache:clear
-
-# Clear and cache routes
-php artisan route:clear
-php artisan route:cache
-
-# Clear and cache config
-php artisan config:clear
-php artisan config:cache
+# TODO
+# php artisan db:wipe
+# php artisan db:restore
+php artisan migrate
 
 # Clear the old cache
 php artisan clear-compiled
@@ -36,7 +25,7 @@ php artisan optimize
 
 # Compile npm assets
 # npm run prod
-cd vendor/laravel/nova && npm install
+# cd vendor/laravel/nova && npm install
 # Exit maintenance mode
 php artisan up
 
