a212ecb290ec4550463f408ba67294d66865838b
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-15 14:42:33 +0100

    Added public page for Quotes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\QuoteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/quote/{id}', [QuoteController::class, 'show']);
