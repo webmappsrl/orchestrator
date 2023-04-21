67cabbc313e1f38ab78a7b715d69f5024f244098
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-04-21 10:03:56 +0200

    Changed epic relationship to be nullable in story table

---
+++ database/migrations/2023_04_21_080110_change_epic_relation_to_nullable_in_story_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('stories', function (Blueprint $table) {
+            $table->foreignId('epic_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('stories', function (Blueprint $table) {
+            $table->foreignId('epic_id')->change();
+        });
+    }
+};
