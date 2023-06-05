ff23a5f9bbc553466f06b83f31276aa1fbda67dc
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-06-05 12:50:20 +0200

    Deadline Nova Resource

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -15,6 +15,7 @@ use App\Nova\DoneEpic;
 use App\Nova\TestEpic;
 use Laravel\Nova\Nova;
 use App\Enums\UserRole;
+use App\Nova\Deadline;
 use App\Nova\Milestone;
 use App\Nova\ProjectEpic;
 use App\Nova\ProgressEpic;
@@ -62,6 +63,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
                     MenuItem::resource(Product::class),
                     MenuItem::resource(RecurringProduct::class),
                     MenuItem::resource(Quote::class),
+                    MenuItem::resource(Deadline::class)
                 ])->icon('users')->collapsable(),
 
                 MenuSection::make('DEV', [
