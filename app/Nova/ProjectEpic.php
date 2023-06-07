22d301bf10786ab44f17dd18cfcc61d6dfe2dbc9
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-06-07 16:13:28 +0200

    Epic resource various: views updated

--- app/Nova/ProjectEpic.php
+++ app/Nova/ProjectEpic.php
@@ -2,24 +2,31 @@
 
 namespace App\Nova;
 
+use Eminiarts\Tabs\Tab;
 use Laravel\Nova\Panel;
+use Eminiarts\Tabs\Tabs;
 use App\Enums\EpicStatus;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Status;
 use Laravel\Nova\Fields\HasMany;
+use Eminiarts\Tabs\Traits\HasTabs;
 use Laravel\Nova\Fields\BelongsTo;
 use App\Nova\Actions\EpicDoneAction;
+use Laravel\Nova\Fields\MorphToMany;
+use Laravel\Nova\Fields\MultiSelect;
+use Laravel\Nova\Fields\BelongsToMany;
 use App\Nova\Actions\EditEpicsFromIndex;
 use Datomatic\NovaMarkdownTui\MarkdownTui;
 use App\Nova\Actions\CreateStoriesFromText;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Datomatic\NovaMarkdownTui\Enums\EditorType;
-
 use Khalin\Nova4SearchableBelongsToFilter\NovaSearchableBelongsToFilter;
-use Laravel\Nova\Fields\BelongsToMany;
 
 class ProjectEpic extends Resource
 {
+    use HasTabs;
     /**
      * The model the resource corresponds to.
      *
@@ -47,7 +54,7 @@ class ProjectEpic extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'description', 'milestone.name', 'user.name', 'project.name' // <-- searchable by project name
+        'id', 'name', 'description', 'milestone.name', 'user.name', 'project.name', 'status'
     ];
 
     /**
@@ -71,44 +78,63 @@ class ProjectEpic extends Resource
     public function fields(NovaRequest $request)
     {
         return [
-
-            new Panel('MAIN INFO', [
-                ID::make()->sortable(),
-                //display the relations in nova field
-                BelongsTo::make('User'),
-                BelongsTo::make('Milestone'),
-                BelongsTo::make('Project')->searchable(),
-                Text::make('Name')
-                    ->sortable()
-                    ->rules('required', 'max:255')
-                    ->onlyOnIndex()
-                    ->displayUsing(function ($name, $a, $b) {
-                        $wrappedName = wordwrap($name, 50, "\n", true);
-                        $htmlName = str_replace("\n", '<br>', $wrappedName);
-                        return $htmlName;
-                    })
-                    ->asHtml(),
-                Text::make('Name')
-                    ->sortable()
-                    ->rules('required', 'max:255')
-                    ->hideFromIndex(),
-                Text::make('SAL', function () {
-                    return $this->wip();
-                })->hideWhenCreating()->hideWhenUpdating(),
-                Text::make('URL', 'pull_request_link')->nullable()->hideFromIndex()->displayUsing(function () {
-                    return '<a class="link-default" target="_blank" href="' . $this->pull_request_link . '">' . $this->pull_request_link . '</a>';
-                })->asHtml(),
-                Text::make('Status')->onlyOnDetail(),
-
-            ]),
-
+            (new Tabs('Info', [
+                Tab::make('Main Info', [
+                    ID::make()->sortable(),
+                    //display the relations in nova field
+                    BelongsTo::make('User'),
+                    BelongsTo::make('Milestone'),
+                    BelongsTo::make('Project')->searchable(),
+                    Text::make('Name')
+                        ->sortable()
+                        ->rules('required', 'max:255')
+                        ->onlyOnIndex()
+                        ->displayUsing(function ($name, $a, $b) {
+                            $wrappedName = wordwrap($name, 50, "\n", true);
+                            $htmlName = str_replace("\n", '<br>', $wrappedName);
+                            return $htmlName;
+                        })
+                        ->asHtml(),
+                    Text::make('Name')
+                        ->sortable()
+                        ->rules('required', 'max:255')
+                        ->onlyOnForms(),
+                    Text::make('SAL', function () {
+                        return $this->wip();
+                    })->hideWhenCreating()->hideWhenUpdating(),
+                    Text::make('URL', 'pull_request_link')->nullable()->hideFromIndex()->displayUsing(function () {
+                        return '<a class="link-default" target="_blank" href="' . $this->pull_request_link . '">' . $this->pull_request_link . '</a>';
+                    })->asHtml(),
+                    Select::make('Status', 'status')
+                        ->options([
+                            'new' => EpicStatus::New,
+                            'project' => EpicStatus::Project,
+                            'progress' => EpicStatus::Progress,
+                            'testing' => EpicStatus::Test,
+                            'rejected' => EpicStatus::Rejected,
+                            'done' => EpicStatus::Done,
+                        ])
+                        ->rules('required')
+                        ->onlyOnForms()
+                        ->default('project'),
+                    Status::make('Status')
+                        ->loadingWhen(['status' => 'project'])
+                        ->failedWhen(['status' => 'rejected'])
+                ]),
+                Tab::make('Stories', [
+                    HasMany::make('Stories'),
+                ]),
+                Tab::make('Deadlines', [
+                    MorphToMany::make('Deadlines'),
+                ]),
+                Tab::make('Tag projects', [
+                    BelongsToMany::make('Tag projects', 'tagProjects', 'App\Nova\Project')->searchable(),
+                ]),
+            ]))->withToolbar(),
             new Panel('DESCRIPTION', [
                 MarkdownTui::make('Description')
-                    ->hideFromIndex()
                     ->initialEditType(EditorType::MARKDOWN)
             ]),
-            HasMany::make('Stories'),
-            BelongsToMany::make('Tag projects','tagProjects','App\Nova\Project')->searchable()
         ];
     }
 
@@ -148,7 +174,9 @@ class ProjectEpic extends Resource
      */
     public function lenses(NovaRequest $request)
     {
-        return [];
+        return [
+            new Lenses\MyEpicLens,
+        ];
     }
 
     /**
@@ -172,6 +200,7 @@ class ProjectEpic extends Resource
                 ->cancelButtonText('Cancel'),
         ];
     }
+
     public function indexBreadcrumb()
     {
         return null;
