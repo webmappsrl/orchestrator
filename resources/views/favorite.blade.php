50cd7bf3dc2ef519de97c382ad6143a501a8b623
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-06-13 17:11:50 +0200

    added styling to links

--- resources/views/favorite.blade.php
+++ resources/views/favorite.blade.php
@@ -11,17 +11,22 @@
 <html>
 
 <body>
-    <h1 style="margin-bottom: 10px; font-weight: bold;">Favorite Projects</h1>
+    <div style="padding:10px;">
+        <h1 style="margin-bottom: 10px; font-weight: bold;">Favorite Projects</h1>
 
-    <ul>
-        @foreach ($favoriteProjects as $project)
-            <li style="margin-bottom:5px; color:lightseagreen">
-                <a href="{{ $urlNova . '/' . $project->id }}">
-                    {{ $project->name }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+        <ul>
+            @foreach ($favoriteProjects as $project)
+                <li onmouseover="this.style.color = 'green'; this.style.fontWeight = 'bold';"
+                    onmouseout="this.style.color = 'lightseagreen';  this.style.fontWeight = 'normal';"
+                    style="margin-bottom:5px; color:lightseagreen; font-size:14px; transition: all .2s ease-out;">
+                    <a href="{{ $urlNova . '/' . $project->id }}">
+                        {{ $project->name }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
 </body>
 
 </html>
