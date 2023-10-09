0e77e5de3415f17972e365d777e7a6f34d72cc30
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2023-10-09 16:12:34 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,9 +13,9 @@ return [
     |
     */
 
-    'version' => '1.11.0',
-    'release' => '1.11.0',
-    'release_date' => '26/09/2023',
+    'version' => '1.12.0',
+    'release' => '1.12.0',
+    'release_date' => '09/10/2023',
 
 
     /*
