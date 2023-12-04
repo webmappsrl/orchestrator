5361f6c88f05b8a47854874dcff4db666b1891a2
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-12-04 15:39:59 +0100

    fix: archived story indexquery

--- app/Nova/ArchivedStories.php
+++ app/Nova/ArchivedStories.php
@@ -13,6 +13,6 @@ class ArchivedStories extends Story
     public static function indexQuery(NovaRequest $request, $query)
     {
         $allowedStatuses = [StoryStatus::Done, StoryStatus::Rejected];
-        return $query->whereIn('status', $allowedStatuses);
+        return $query->whereIn('status', $allowedStatuses)->where('user_id', $request->user()->id);
     }
 }
