77cb60dae3a3e352904cfe4a295ac931d3bb60d7
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-08 12:03:51 +0100

    feat: created orchestrator config file

---
+++ config/orchestrator.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'story' => [
+        'status' => [
+            'color-mapping' => [
+                'new' => 'rgb(0, 0, 255)', // Blue
+                'progress' => 'rgb(204, 204, 0)', // Yellow
+                'testing' => 'rgb(255, 165, 0)', // Orange
+                'rejected' => 'rgb(255, 0, 0)', // Red
+                'done' => 'rgb(0, 128, 0)' // Green
+            ],
+        ],
+    ],
+];
