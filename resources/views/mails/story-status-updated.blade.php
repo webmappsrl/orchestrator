14e1f2bba59b4876761b58938b6ecc835531f376
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-09 08:44:37 +0100

    feat: added due_date to deadline in story status update mail

--- resources/views/mails/story-status-updated.blade.php
+++ resources/views/mails/story-status-updated.blade.php
@@ -6,9 +6,9 @@ Ciao {{ $user->name }},
 Lo stato di una storia a te assegnata come {{$userType}} è stato aggiornato a <span style="background-color:{{ $colorMapping[$status] ?? 'black' }}; color: white; padding: 2px 4px;">{{ $status }}</span>.
 
 @if (count($story->deadlines) > 0)
-La storia è inserita nella deadline : [{{ $story->deadlines->first()->title }}]( {{ url('resources/deadlines/' . $story->deadlines->first()->id) }} ).
-    
+La storia è inserita nella deadline : [{{ $story->deadlines->first()->title }}]( {{ url('resources/deadlines/' . $story->deadlines->first()->id) }} ) con scadenza: {{$story->deadlines->first()->due_date->format('d-m-Y')}}.    
 @endif
+
 Puoi visualizzare i dettagli della storia a questo [link]({{ url('resources/stories/' . $story->id) }}).
 
 Grazie,<br>
