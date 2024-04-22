7a35333461bb627122297b05a79fd58bc35a9dbb
bongiu <peppedeka@gmail.com>
2024-04-22 10:49:11 +0200

    feat: Add RedirectIfCustomer middleware
    This commit adds a new middleware called RedirectIfCustomer. This middleware is responsible for redirecting authenticated customers to specific routes based on their role. It checks if the user is authenticated and has the role of 'customer'. If so, it redirects them to the appropriate route based on the requested URL. The middleware also logs information about the authentication status and the default guard in use.
    
    Co-authored-by: [Author Name] <[Author Email]>

---
+++ app/Http/Middleware/RedirectIfCustomer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Enums\UserRole;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfCustomer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info('Guard in use: ' . Auth::getDefaultDriver());
+        Log::info('User authenticated: ' . Auth::check());
+        $customerRole = UserRole::Customer;
+        $user = $request->user();
+        // Controlla se l'utente è autenticato e ha il ruolo di 'customer'
+        if (isset($user) && $user->hasRole($customerRole)) {
+            // Reindirizza a 'customerStory' se l'utente è un 'customer'
+            if ($request->is('resources/stories')) {
+                return redirect('resources/story-showed-by-customers');
+            } elseif ($request->is('resources/stories/*')) {
+                // Estrai l'ID della storia dalla URL
+                $path = $request->path(); // es. 'resources/stories/3084'
+                $pathParts = explode('/', $path);
+                $id = end($pathParts);
+                // Reindirizza alla nuova URL includendo l'ID
+                return redirect('resources/story-showed-by-customers/' . $id);
+            }
+        }
+
+        return $next($request);
+    }
+}
