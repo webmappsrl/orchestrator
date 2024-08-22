ca351219fa1fe30e6724012ce0694f0e0c2bad59
Gennaro Manzo <75580783+gemanzo@users.noreply.github.com>
2024-08-22 08:29:21 +0200

    Feat: email to ticket OC:3722 (#74)
    * feat: process inbound emails job v1
    
    * feat: added mail to user when not found on orchestrator
    
    * feat: added mail send to user when creating ticket from mail
    
    * chore: added route testing middleware for local development only
    
    * feat: check emails job dedicated log channel
    
    * chore: added artisan command wrapper for the job

---
+++ app/Jobs/ProcessInboundEmails.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Models\Story;
+use App\Enums\StoryStatus;
+use Illuminate\Bus\Queueable;
+use Webklex\IMAP\Facades\Client;
+use App\Mail\CustomerStoryFromMail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrchestratorUserNotFound;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ProcessInboundEmails implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct()
+    {
+        $this->logger = Log::channel('process_inbound_emails');
+    }
+
+    public function handle()
+    {
+        $client = Client::account('default');
+
+        $client->connect();
+
+        $folder = $client->getFolder('INBOX');
+
+        // Recupera tutte le email non lette (senza il flag "Seen")
+        $messages = $folder->query()->unseen()->get();
+        $this->logger->info('Numero di email da elaborare: ' . $messages->count());
+
+        if ($messages->count() == 0) {
+            $this->logger->info('Nessun nuovo messaggio da elaborare');
+            return;
+        }
+
+        foreach ($messages as $message) {
+            $story = $this->createOrchestratorStoryFromMail($message);
+            $attachments = $message->hasAttachments() ? $message->getAttachments() : [];
+
+            if ($story) {
+                if (count($attachments) > 0) {
+                    foreach ($attachments as $attachment) {
+                        $this->associateAttachment($attachment, $story);
+                    }
+                }
+                // Segna la email come letta soltanto se viene creata la story
+                $message->setFlag('Seen');
+            }
+        }
+        $client->disconnect();
+    }
+
+    private function createOrchestratorStoryFromMail($message)
+    {
+        try {
+            $userEmail = $message->getFrom()[0]->mail;
+            $subject = $message->getSubject();
+            $body = $message->hasTextBody() ? $message->getTextBody() : $message->getBodies();
+
+            $user = User::where('email', $userEmail)->first();
+
+            if ($user) {
+                // Creazione della nuova story
+                $story = new Story();
+                $story->user_id = $user->id;
+                $story->name = $subject;
+                $story->description = $body;
+                $story->status = StoryStatus::New;
+                $story->creator_id = $user->id;
+                $story->save();
+                $this->logger->info('Story ID ' . $story->id . ' creata.');
+                Mail::to($userEmail)->send(new CustomerStoryFromMail($story));
+                return $story;
+            } else {
+                $this->logger->warning("Nessun utente trovato per l'email: $userEmail");
+                //send email to the user email to notify that the user was not registered on orchestrator and that he must contact info@webmapp.it
+                Mail::to($userEmail)->send(new OrchestratorUserNotFound($subject));
+                return null;
+            }
+        } catch (\Exception $e) {
+            $this->logger->error('Error creating story: ' . $e->getMessage());
+        }
+    }
+
+    private function associateAttachment($attachment, $story)
+    {
+        $attachmentName = $attachment->getName();
+        $mimeType = $attachment->getMimeType();
+
+
+        // Salva l'allegato in una directory temporanea
+        try {
+            Storage::put('public/' . $attachmentName, $attachment->getContent());
+
+            $temporaryPath = Storage::path('public/' . $attachmentName);
+            $this->logger->info("Percorso temporaneo: " . $temporaryPath);
+
+            // Controlla se il file esiste
+            if (Storage::exists('public/' . $attachmentName)) {
+                $this->logger->info("File temporaneo trovato: " . $temporaryPath);
+            } else {
+                $this->logger->error("File temporaneo non trovato: " . $temporaryPath);
+            }
+
+
+            // Verifica il MIME type per decidere la collezione corretta
+            if (in_array($mimeType, config('services.media-library.allowed_document_formats'))) {
+                $this->logger->info("File documento: " . $temporaryPath . " MIME type: $mimeType");
+                // Se è un documento, aggiungilo alla collezione documents
+                $story->addMedia($temporaryPath)
+                    ->toMediaCollection('documents');
+            } elseif (in_array($mimeType, config('services.media-library.allowed_image_formats'))) {
+                $this->logger->info("File immagine: " . $temporaryPath . " MIME type: $mimeType, potrebbero esserci problemi nella visualizzazione Nova");
+                // Se è un'immagine, aggiungilo alla collezione images
+                $story->addMedia($temporaryPath)
+                    ->toMediaCollection('images');
+            } else {
+                // MIME type non supportato, logga un avviso
+                $this->logger->warning("Allegato non supportato: $attachmentName con MIME type $mimeType");
+            }
+
+            // Rimuovi il file temporaneo
+            Storage::delete('public/' . $attachmentName);
+        } catch (\Exception $e) {
+            $this->logger->error('Error associating attachment: ' . $e->getMessage());
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Models\User;
+use App\Jobs\TestJob;
+use App\Models\Story;
+use App\Mail\CustomerStoryFromMail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
+use App\Mail\OrchestratorUserNotFound;
 use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\DeadlineController;
-use App\Jobs\TestJob;
-use Illuminate\Support\Facades\Redis;
+use App\Http\Middleware\TestRouteAccess;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,7 +32,62 @@ Route::get('/mailable', function () {
     $user = User::whereJsonContains('roles', 'customer')->firstOrFail();
     return new App\Mail\CustomerStoriesDigest($user);
 });
-Route::get('/test-horizon', function () {
-    TestJob::dispatch();
-    return 'Test job dispatched';
+
+
+//testing routes
+Route::middleware(TestRouteAccess::class)->group(function () {
+
+    Route::get('/test-horizon', function () {
+        TestJob::dispatch();
+        return 'Test job dispatched';
+    });
+
+    // Rotta per testare l'email CustomerStoryFromMail
+    Route::get('/test-customer-story-email', function () {
+        // Creazione di una nuova istanza della mail
+        $story = Story::inRandomOrder()->first();
+        $email = new CustomerStoryFromMail($story);
+        $environment = env('APP_ENV');
+
+        // Invio dell'email a un indirizzo di test
+        Mail::to(config('mail.from.address'))->send($email);
+
+        return 'Email di test per CustomerStoryFromMail inviata!';
+    });
+
+    // Rotta per testare l'email OrchestratorUserNotFound
+    Route::get('/test-user-not-found-email', function () {
+        // Creazione di una nuova istanza della mail con un soggetto di esempio
+        $subject = 'Test di segnalazione utente non trovato';
+        $email = new OrchestratorUserNotFound($subject);
+
+        // Invio dell'email a un indirizzo di test
+        Mail::to('gennaromanzo@webmapp.it')->send($email);
+
+        return 'Email di test per OrchestratorUserNotFound inviata!';
+    });
+
+    // Rotta per testare il rendering dell'email CustomerStoryFromMail
+    Route::get('/test-render-customer-story-email', function () {
+        // Creazione di una nuova istanza della mail con una storia di esempio
+        $story = Story::inRandomOrder()->first(); // Assicurati che ci sia almeno una storia nel database
+        $email = new CustomerStoryFromMail($story);
+
+        // Rendering della vista della mail
+        $view = View::make($email->content()->view, ['story' => $story]);
+
+        return $view->render();
+    });
+
+    // Rotta per testare il rendering dell'email OrchestratorUserNotFound
+    Route::get('/test-render-user-not-found-email', function () {
+        // Creazione di una nuova istanza della mail con un soggetto di esempio
+        $subject = 'Test di segnalazione utente non trovato';
+        $email = new OrchestratorUserNotFound($subject);
+
+        // Rendering della vista della mail
+        $view = View::make($email->content()->view, ['sub' => $subject]);
+
+        return $view->render();
+    });
 });
