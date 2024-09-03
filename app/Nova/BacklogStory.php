5ad8aa0d2c12d17dd776a51067c752d9c832024a
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-09-03 15:19:04 +0200

    feat: added backlog stories OC:3884

--- app/Nova/BacklogStory.php
+++ app/Nova/BacklogStory.php
@@ -8,29 +8,30 @@ use App\Enums\StoryType;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Query\Search\SearchableRelation;
 
-class CustomerFeatureStory extends Story
+class BacklogStory extends Story
 {
 
     public $hideFields = ['updated_at'];
 
     public static function label()
     {
-        return __('Feature stories');
+        return __('Backlog stories');
     }
 
 
     public static function searchableColumns()
     {
         return [
-            'id', 'name', new SearchableRelation('creator', 'name'),
+            'id',
+            'name',
+            new SearchableRelation('creator', 'name'),
         ];
     }
 
     public static function indexQuery(NovaRequest $request, $query)
     {
         return $query->whereNotNull('creator_id')
-            ->where('status', '!=', StoryStatus::Done->value)
-            ->where('type', '=', StoryType::Feature->value);
+            ->where('status', StoryStatus::Backlog->value);
     }
 
     public function cards(NovaRequest $request)
