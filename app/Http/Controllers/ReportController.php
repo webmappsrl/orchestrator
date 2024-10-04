e4e44680565b2b54ba87bb5f3a4d75d4a901c16f
bongiu <peppedeka@gmail.com>
2024-10-04 11:47:48 +0200

    chore: Refactor ReportController.php for improved readability and maintainability
    - Extracted private functions to improve code organization
    - Renamed variables and functions for clarity
    - Reordered code blocks for better flow

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -39,9 +39,98 @@ class ReportController extends Controller
 
         return view('reports.index', compact('tab1Type', 'tab2Status', 'tab2StatusTotals', 'year', 'availableQuarters', 'tab3DevStatus', 'developers', 'tab4StatusDev', 'tab5CustomerStatus', 'tab6StatusCustomer', 'tab7TagCustomer', 'tab8CustomerTag', 'tab9TagType'));
     }
-    /**
-     * Genera il report per Tipo di Storia
-     */
+    private function generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn)
+    {
+        $quarterReport = [];
+        $quarterReport['thead'] = $thead;
+        $quarterReport['tbody'] = [];
+        $tbody['year'] = $this->calculateRowData($year, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
+        foreach ($availableQuarters as $quarter) {
+            $tbody['q' . $quarter] = $this->calculateRowData($year, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn, $quarter);
+        }
+        $quarterReport['tbody'] =   $tbody;
+
+        return $quarterReport;
+    }
+
+    private function getYearAndQuarters($year)
+    {
+        $currentYear = Carbon::now()->year;
+        $currentQuarter = Carbon::now()->quarter;
+        if (!$year) {
+            return ['All Time', [1, 2, 3, 4], null]; // Nessun errore, tutti i quarter sono disponibili
+        }
+        if ($year > $currentYear) {
+            return [$year, [], 'Nessun dato disponibile per il futuro.'];
+        }
+        $availableQuarters = $year == $currentYear ? range(1, $currentQuarter) : [1, 2, 3, 4];
+
+        return [$year, $availableQuarters, null]; // Nessun errore
+    }
+
+    private function getDevelopers()
+    {
+        $developers = User::whereJsonContains('roles', UserRole::Developer)
+            ->whereHas('stories')  // Verifica che l'utente abbia storie associate
+            ->distinct()
+            ->get();
+        return $developers;
+    }
+    private function getCustomers()
+    {
+        return Ticket::whereNotNull('creator_id')
+            ->whereHas('creator', function ($query) {
+                $query->whereJsonContains('roles', UserRole::Customer); // Filtra utenti con il ruolo 'Customer'
+            })
+            ->selectRaw('creator_id, COUNT(*) as story_count') // Seleziona il creator_id e conta le storie
+            ->groupBy('creator_id') // Raggruppa per creator_id
+            ->orderByDesc('story_count') // Ordina per il numero di storie in modo decrescente
+            ->limit(10) // Limita ai primi 10
+            ->with('creator') // Precarica il creatore
+            ->get()
+            ->pluck('creator') // Ottiene solo i creatori
+            ->unique('id'); // Rimuovi eventuali duplicati, se ce ne sono
+
+    }
+    private function getTags()
+    {
+        return Tag::withCount('tagged') // Conta quante storie sono associate a ciascun tag
+            ->orderBy('tagged_count', 'desc') // Ordina per frequenza di utilizzo
+            ->limit(10) // Limita ai primi 10 tag più usati
+            ->get();
+    }
+
+    private function calculateRowData($year, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn, $quarter = null)
+    {
+        $rows = [];
+        foreach ($firstColumnCells as $indexRowObj) {
+            $row = [];
+            foreach ($thead as $indexColumnObj) {
+                if ($indexColumnObj === '') {
+                    $row[] = $firstColumnNameFn($indexRowObj, $indexColumnObj);
+                } elseif ($indexColumnObj === 'totale') {
+                    $row[] = array_sum(array_slice($row, 1));
+                } else {
+                    $query = $cellQueryFn($indexRowObj, $indexColumnObj);
+                    if ($quarter) {
+                        $query->whereRaw('EXTRACT(QUARTER FROM updated_at) = ?', [$quarter]);
+                    }
+                    if ($year !== 'All Time') {
+                        $query->whereYear('updated_at', $year);
+                    }
+                    $statusTotal = $query->count();
+                    $row[] = $statusTotal;
+                }
+            }
+            $rows[] = $row;
+        }
+        usort($rows, function ($a, $b) {
+            return $b[count($a) - 1] <=> $a[count($a) - 1];
+        });
+
+        return $rows;
+    }
+
     private function tab1Type($year, $availableQuarters)
     {
         $totalStories = $year === 'All Time' ? Ticket::count() : Ticket::whereYear('updated_at', $year)->count();
@@ -78,9 +167,7 @@ class ReportController extends Controller
 
         return $tab1Type;
     }
-    /**
-     * Genera il report per Stato di Storia
-     */
+
     private function tab2Status($year, $availableQuarters)
     {
         $totalStories = $year === 'All Time' ? Ticket::count() : Ticket::whereYear('updated_at', $year)->count();
@@ -132,237 +219,127 @@ class ReportController extends Controller
 
         return [$tab2Status, $totals]; // Restituisci anche i totali
     }
-    /**
-     * Funzione per determinare l'anno e i quarter disponibili
-     */
-    private function getYearAndQuarters($year)
-    {
-        $currentYear = Carbon::now()->year;
-        $currentQuarter = Carbon::now()->quarter;
-
-        // Se non viene passato un anno, visualizza "All Time" e considera tutti i dati disponibili
-        if (!$year) {
-            return ['All Time', [1, 2, 3, 4], null]; // Nessun errore, tutti i quarter sono disponibili
-        }
-
-        // Se l'anno passato è nel futuro, restituiamo un errore
-        if ($year > $currentYear) {
-            return [$year, [], 'Nessun dato disponibile per il futuro.'];
-        }
-
-        // Se l'anno è quello corrente, restituiamo solo i quarter fino al corrente
-        $availableQuarters = $year == $currentYear ? range(1, $currentQuarter) : [1, 2, 3, 4];
-
-        return [$year, $availableQuarters, null]; // Nessun errore
-    }
-
-    private function getDevelopers()
-    {
-        $developers = User::whereJsonContains('roles', UserRole::Developer)
-            ->whereHas('stories')  // Verifica che l'utente abbia storie associate
-            ->distinct()
-            ->get();
-        return $developers;
-    }
-    private function getCustomers()
-    {
-        return Ticket::whereNotNull('creator_id')
-            ->whereHas('creator', function ($query) {
-                $query->whereJsonContains('roles', UserRole::Customer); // Filtra utenti con il ruolo 'Customer'
-            })
-            ->selectRaw('creator_id, COUNT(*) as story_count') // Seleziona il creator_id e conta le storie
-            ->groupBy('creator_id') // Raggruppa per creator_id
-            ->orderByDesc('story_count') // Ordina per il numero di storie in modo decrescente
-            ->limit(10) // Limita ai primi 10
-            ->with('creator') // Precarica il creatore
-            ->get()
-            ->pluck('creator') // Ottiene solo i creatori
-            ->unique('id'); // Rimuovi eventuali duplicati, se ce ne sono
-
-    }
-    private function getTags()
-    {
-        return Tag::withCount('tagged') // Conta quante storie sono associate a ciascun tag
-            ->orderBy('tagged_count', 'desc') // Ordina per frequenza di utilizzo
-            ->limit(10) // Limita ai primi 10 tag più usati
-            ->get();
-    }
-
-    private function calculateRowData($year, $firstColumnCells, $thead, $nameFn, $queryFn, $quarter = null)
-    {
-        $rows = [];
-        foreach ($firstColumnCells as $cell) {
-            $row = [];
-            foreach ($thead as $column) {
-                if ($column === '') {
-                    $row[] = $nameFn($cell, $column);
-                } elseif ($column === 'totale') {
-                    $totalPerUser = array_sum(array_slice($row, 1)); // Somma dei valori per gli stati
-                    $row[] = $totalPerUser;
-                } else {
-                    $query = $queryFn($cell, $column);
-
-                    if ($quarter) {
-                        // Filtra per quarter se fornito
-                        $query->whereRaw('EXTRACT(QUARTER FROM updated_at) = ?', [$quarter]);
-                    }
-
-                    if ($year !== 'All Time') {
-                        $query->whereYear('updated_at', $year);
-                    }
-
-                    $statusTotal = $query->count();
-
-                    // Aggiungi il totale per lo stato corrente
-                    $row[] = $statusTotal;
-                }
-            }
-            $rows[] = $row;
-        }
-        usort($rows, function ($a, $b) {
-            return $b[count($a) - 1] <=> $a[count($a) - 1]; // Ordina in base all'ultima colonna (totale)
-        });
-
-        return $rows; // Restituisce un array di righe che segue l'ordine di thead
-    }
-
 
     private function tab3DevStatus($year, $availableQuarters, $developers)
     {
-        $queryFn = function ($cell, $column) {
-            return   Ticket::where('user_id', $cell->id)
-                ->where('status', $column);
+        $cellQueryFn = function ($indexRowObj, $indexColumnObj) {
+            return   Ticket::where('user_id', $indexRowObj->id)
+                ->where('status', $indexColumnObj);
         };
-        $nameFn = function ($cell) {
-            return $cell->name;
+        $firstColumnNameFn = function ($indexRowObj) {
+            return $indexRowObj->name;
         };
         $thead = array_merge([''], StoryStatus::values(), ['totale']);
         $firstColumnCells = $developers;
 
-        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn);
-    }
-    private function tab5CustomerStatus($year, $availableQuarters, $customers)
-    {
-        $queryFn = function ($cell, $column) {
-            return   Ticket::where('creator_id', $cell->id)
-                ->where('status', $column);
-        };
-        $nameFn = function ($cell) {
-            return $cell->name;
-        };
-        $thead = array_merge([''], StoryStatus::values(), ['totale']);
-        $firstColumnCells = $customers;
-
-        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn);
-    }
-    private function tab7TagCustomer($year, $availableQuarters, $tags, $customers)
-    {
-        $queryFn = function ($tag, $column) {
-            // Query per contare quante storie hanno il tag specificato e lo stato specificato
-            return Ticket::whereHas('tags', function ($query) use ($tag) {
-                $query->where('tags.id', $tag->id); // Filtra per il tag specifico
-            })
-                ->whereHas('creator', function ($q) use ($column) {
-                    $q->where('name', $column); // Filtra per il nome dell'utente nel campo 'column'
-                });
-        };
-        $nameFn = function ($cell) {
-            return $cell->name;
-        };
-        $thead = array_merge([''], $customers->pluck('name')->toArray(), ['totale']);
-        $firstColumnCells = $tags;
-
-        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn);
+        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
     }
 
     private function tab4StatusDev($year, $availableQuarters, $developers)
     {
-        $thead = array_merge([''], $developers->pluck('name')->toArray(), ['totale']);
-        $queryFn = function ($cell, $column) {
-            return     Ticket::where('status', $cell)
-                ->whereHas('user', function ($q) use ($column) {
-                    $q->where('name', $column); // Filtra per il nome dell'utente nel campo 'column'
+        $cellQueryFn = function ($indexRowObj, $indexColumnObj) {
+            return     Ticket::where('status', $indexRowObj)
+                ->whereHas('user', function ($q) use ($indexColumnObj) {
+                    $q->where('name', $indexColumnObj); // Filtra per il nome dell'utente nel campo 'column'
                 });
         };
-        $nameFn = function ($cell, $column) {
-            return $cell ?? 'non assegnato';
+        $firstColumnNameFn = function ($indexRowObj, $indexColumnObj) {
+            return $indexRowObj ?? 'non assegnato';
         };
+        $thead = array_merge([''], $developers->pluck('name')->toArray(), ['totale']);
         $firstColumnCells = StoryStatus::values();
 
-        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn);
+        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
+    }
+
+    private function tab5CustomerStatus($year, $availableQuarters, $customers)
+    {
+        $cellQueryFn = function ($indexRowObj, $indexColumnObj) {
+            return   Ticket::where('creator_id', $indexRowObj->id)
+                ->where('status', $indexColumnObj);
+        };
+        $firstColumnNameFn = function ($indexRowObj) {
+            return $indexRowObj->name;
+        };
+        $thead = array_merge([''], StoryStatus::values(), ['totale']);
+        $firstColumnCells = $customers;
+
+        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
     }
 
     private function tab6StatusCustomer($year, $availableQuarters, $customer)
     {
-        $thead = array_merge([''], $customer->pluck('name')->toArray(), ['totale']);
-        $queryFn = function ($cell, $column) {
-            return     Ticket::where('status', $cell)
-                ->whereHas('creator', function ($q) use ($column) {
-                    $q->where('name', $column); // Filtra per il nome dell'utente nel campo 'column'
+        $cellQueryFn = function ($indexRowObj, $indexColumnObj) {
+            return     Ticket::where('status', $indexRowObj)
+                ->whereHas('creator', function ($q) use ($indexColumnObj) {
+                    $q->where('name', $indexColumnObj); // Filtra per il nome dell'utente nel campo 'column'
                 });
         };
-        $nameFn = function ($cell, $column) {
-            return $cell ?? 'non assegnato';
+        $firstColumnNameFn = function ($indexRowObj, $indexColumnObj) {
+            return $indexRowObj ?? 'non assegnato';
         };
+        $thead = array_merge([''], $customer->pluck('name')->toArray(), ['totale']);
         $firstColumnCells = StoryStatus::values();
 
-        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn);
+        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
     }
+
+    private function tab7TagCustomer($year, $availableQuarters, $tags, $customers)
+    {
+        $cellQueryFn = function ($tag, $indexColumnObj) {
+            // Query per contare quante storie hanno il tag specificato e lo stato specificato
+            return Ticket::whereHas('tags', function ($query) use ($tag) {
+                $query->where('tags.id', $tag->id); // Filtra per il tag specifico
+            })
+                ->whereHas('creator', function ($q) use ($indexColumnObj) {
+                    $q->where('name', $indexColumnObj); // Filtra per il nome dell'utente nel campo 'column'
+                });
+        };
+        $firstColumnNameFn = function ($indexRowObj) {
+            return $indexRowObj->name;
+        };
+        $thead = array_merge([''], $customers->pluck('name')->toArray(), ['totale']);
+        $firstColumnCells = $tags;
+
+        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
+    }
+
     private function tab8CustomerTag($year, $availableQuarters, $tags, $customers)
     {
-        $thead = array_merge([''], $tags->pluck('name')->toArray(), ['totale']);
-        $queryFn = function ($cell, $column) use ($year) {
+        $cellQueryFn = function ($indexRowObj, $indexColumnObj) use ($year) {
             return Ticket::whereNotNull('creator_id')
-                ->whereHas('creator', function ($q) use ($cell, $column) {
-                    $q->where('name', $cell->name); // Filtra per il nome dell'utente nel campo 'column'
+                ->whereHas('creator', function ($q) use ($indexRowObj, $indexColumnObj) {
+                    $q->where('name', $indexRowObj->name); // Filtra per il nome dell'utente nel campo 'column'
                 }) // Filtra per lo stato specifico
-                ->whereHas('tags', function ($query) use ($cell, $column) {
-                    $query->where('tags.name', $column); // Filtra per il nome del tag
+                ->whereHas('tags', function ($query) use ($indexRowObj, $indexColumnObj) {
+                    $query->where('tags.name', $indexColumnObj); // Filtra per il nome del tag
                 })
             ;
         };
-        $nameFn = function ($cell, $column) {
-            return $cell->name ?? 'non assegnato';
+        $firstColumnNameFn = function ($indexRowObj, $indexColumnObj) {
+            return $indexRowObj->name ?? 'non assegnato';
         };
+        $thead = array_merge([''], $tags->pluck('name')->toArray(), ['totale']);
         $firstColumnCells = $customers;
 
-        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn);
+        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
     }
 
     private function tab9TagType($year, $availableQuarters, $tags, $customers)
     {
-        $thead = array_merge([''], StoryType::values(), ['totale']);
-        $queryFn = function ($cell, $column) use ($year) {
+        $cellQueryFn = function ($indexRowObj, $indexColumnObj) use ($year) {
             return Ticket::whereNotNull('creator_id')
-                ->whereHas('tags', function ($query) use ($cell, $column) {
-                    $query->where('tags.name', $cell->name); // Filtra per il nome del tag
+                ->whereHas('tags', function ($query) use ($indexRowObj, $indexColumnObj) {
+                    $query->where('tags.name', $indexRowObj->name); // Filtra per il nome del tag
                 })
-                ->where('type', $column);
+                ->where('type', $indexColumnObj);
         };
-        $nameFn = function ($cell, $column) {
-            return $cell->name ?? 'non assegnato';
+        $firstColumnNameFn = function ($indexRowObj, $indexColumnObj) {
+            return $indexRowObj->name ?? 'non assegnato';
         };
+        $thead = array_merge([''], StoryType::values(), ['totale']);
         $firstColumnCells = $tags;
 
-        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn);
-    }
-
-
-
-
-    private function generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $nameFn, $queryFn)
-    {
-        // Variabile per contenere i totali degli utenti e per l'intero anno
-        $quarterReport = [];
-        $quarterReport['thead'] = $thead;
-        $quarterReport['tbody'] = [];
-
-        $tbody['year'] = $this->calculateRowData($year, $firstColumnCells, $thead, $nameFn, $queryFn);
-        foreach ($availableQuarters as $quarter) {
-            $tbody['q' . $quarter] = $this->calculateRowData($year, $firstColumnCells, $thead, $nameFn, $queryFn, $quarter);
-        }
-        $quarterReport['tbody'] =   $tbody;
-
-        return $quarterReport;
+        return $this->generateQuarterReport($year, $availableQuarters, $firstColumnCells, $thead, $firstColumnNameFn, $cellQueryFn);
     }
 }
