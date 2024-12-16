cf2efeffbfcb6f386ddd9edd003009c98bf757ee
marco <m.baroncini@cyberway.cloud>
2024-12-16 13:57:42 +0100

    ci: fix deploy oc:4465

--- database/seeders/PhpArtisanUserSeeder.php
+++ database/seeders/PhpArtisanUserSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Enums\UserRole;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PhpArtisanUserSeeder extends Seeder
 {
@@ -14,10 +12,10 @@ class PhpArtisanUserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::firstOrCreate(['email' => 'orchestrator_artisan@webmapp.it'], [
             'name' => 'Php artisan',
             'email' => 'orchestrator_artisan@webmapp.it',
             'password' => bcrypt('a very strong password that probably no one will use'),
-        ])->markEmailAsVerified();
+        ]);
     }
 }
