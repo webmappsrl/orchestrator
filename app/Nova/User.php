7716589b5421aff9e37ab59c7beb4af3cdff44af
FalconClaws059 <83666067+FalconClaws059@users.noreply.github.com>
2025-03-04 14:54:22 +0100

    fix: Unfiltered the right resource oc:5007 (#128)

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -84,7 +84,7 @@ class User extends Resource
             BelongsToMany::make('Apps'),
             HasMany::make('Epics'),
             HasMany::make('Stories'),
-            HasMany::make('Quotes', 'quotes', QuoteNoFilter::class),
+            HasMany::make('Quotes', 'quotes', Quote::class),
             Text::make(__('Favorite Projects'), function () {
                 $projects = [];
                 $userFavorites = $this->getFavoriteItems(Project::class)->get();
