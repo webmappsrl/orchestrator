a309c1561805808c8091d4fdfdaeee665f3622bc
bongiu <peppedeka@gmail.com>
2025-03-05 15:53:18 +0100

    chore: Update SyncStoriesWithGoogleCalendar.php
    - Updated the color IDs for DEFAULT_COLOR_ID and WAITING_COLOR_ID
    - Added a new function to get tested tickets
    - Refactored the code to create events for tickets based on their status and developer ID
    - Added a new function to create event labels for specific statuses
    - Updated the deleteCalendar function to handle cases where the developer does not have a valid email

--- app/Console/Commands/SyncStoriesWithGoogleCalendar.php
+++ app/Console/Commands/SyncStoriesWithGoogleCalendar.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\DB;
 use App\Enums\StoryType;
 use Spatie\GoogleCalendar\GoogleCalendarFactory;
 
+use function PHPUnit\Framework\isNull;
+
 class SyncStoriesWithGoogleCalendar extends Command
 {
     protected $signature = 'sync:stories-calendar {developerEmail?}';
@@ -20,9 +22,9 @@ class SyncStoriesWithGoogleCalendar extends Command
     private $startTime;
     private $currentTimeForDeveloper = [];
 
-    private const DEFAULT_COLOR_ID = '5'; // Yellow
+    private const DEFAULT_COLOR_ID = '8'; // Yellow
     private const TESTING_COLOR_ID = '6'; // Tangerine
-    private const WAITING_COLOR_ID = '8'; // Light Gray
+    private const WAITING_COLOR_ID = '5'; // Light Gray
     private const BUG_COLOR_ID = '11'; // Bold Red
     private const HELPDESK_COLOR_ID = '2'; // Green
     private const FEATURE_COLOR_ID = '1'; // Blue
@@ -39,6 +41,7 @@ class SyncStoriesWithGoogleCalendar extends Command
     {
         $developerId = null;
         $developerEmail = $this->argument('developerEmail');
+        $developerIds = [];
 
         // Trova l'ID del developer usando l'email
         if ($developerEmail) {
@@ -48,115 +51,126 @@ class SyncStoriesWithGoogleCalendar extends Command
                 return;
             }
             $developerId = $developer->id;
+            $developerIds[] = $developerId;
+        } else {
+            // Se non è stato passato un email, prendi tutti gli sviluppatori
+            $developerIds = DB::table('users')->whereJsonContains('roles', 'developer')->pluck('id')->toArray();
         }
-        $todoTickets = $this->getTicketsWithStatus(StoryStatus::Todo->value, $developerId);
-        $testingTickets = $this->getTicketsWithStatus(StoryStatus::Test->value, $developerId);
-        $testedTickets = $this->getTicketsWithStatus(StoryStatus::Tested->value, $developerId);
-        $waitingTickets = $this->getTicketsWithStatus(StoryStatus::Waiting->value, $developerId);
-
-        // Prendi tutti gli ID degli sviluppatori coinvolti
-        $developersInvolved = $todoTickets->pluck('user_id')
-            ->merge($testingTickets->pluck('user_id'))
-            ->merge($testingTickets->pluck('tester_id'))
-            ->merge($testedTickets->pluck('user_id'))
-            ->merge($waitingTickets->pluck('user_id'))
-            ->unique()
-            ->toArray();
-
-        // Cancella tutti gli eventi del calendario di ogni sviluppatore coinvolto
-        foreach($developersInvolved as $developerId){
+        foreach ($developerIds as $developerId) {
             $this->deleteCalendar($developerId);
+            $this->initializeTimeForDeveloper($developerId);
+            $todoTickets = $this->getTicketsWithStatus(StoryStatus::Todo->value, $developerId);
+            if (count($todoTickets) > 0) {
+                $this->createEventsForTickets($todoTickets, $developerId, StoryStatus::Todo->value);
+            }
+            $tobeTestedTickets = $this->getTicketsWithStatus(StoryStatus::Test->value, $developerId);
+            if (count($tobeTestedTickets) > 0) {
+                $this->createEventLabel($developerId, '2BETESTED', $this->currentTimeForDeveloper[$developerId]);
+                $this->createEventsForTickets($tobeTestedTickets, $developerId);
+            }
+            $testedTickets = $this->getTestedTickets(StoryStatus::Tested->value, $developerId);
+            if (count($testedTickets) > 0) {
+                $this->createEventLabel($developerId, 'TESTED', $this->currentTimeForDeveloper[$developerId]);
+                $this->createEventsForTickets($testedTickets, $developerId);
+            }
+            $waitingTickets = $this->getTicketsWithStatus(StoryStatus::Waiting->value, $developerId);
+            if (count($waitingTickets) > 0) {
+                $this->createEventLabel($developerId, 'WAITING', $this->currentTimeForDeveloper[$developerId]);
+                $this->createEventsForTickets($waitingTickets, $developerId);
+            }
         }
-
-        // Inizializza il tempo per ogni sviluppatore
-        $this->initializeTimeForAllDevelopers($developersInvolved);
-
-        // Crea gli eventi per i ticket di todo
-        $todoTickets = $todoTickets->groupBy('user_id');
-        $this->createEventsForTickets($todoTickets);
-        
-        // Pausa di 30 minuti per separare i ticket di testing
-        // Aggiorna il tempo per ogni sviluppatore
-        if($todoTickets->count() > 0){
-            $this->add30MinutesToAllDevelopers();
-        }
-
-        // Crea gli eventi per i ticket di testing
-        $testingTickets = $testingTickets->groupBy('tester_id');
-        $this->createEventsForTickets($testingTickets);
-        // Crea gli eventi per i ticket di tested
-        $testedTickets = $testedTickets->groupBy('user_id');
-        $this->createEventsForTickets($testedTickets);
-
-        // Crea gli eventi per i ticket di waiting
-        $waitingTickets = $waitingTickets->groupBy('user_id');
-        $this->createEventsForTickets($waitingTickets);
 
         $this->info('All stories have been synced to Google Calendar');
     }
 
-    public function initializeTimeForAllDevelopers($developersInvolved): void{
-        foreach($developersInvolved as $developerId){
-            $this->currentTimeForDeveloper[$developerId] = $this->startTime->copy();
+    public function initializeTimeForDeveloper($developerId): void
+    {
+        $this->currentTimeForDeveloper[$developerId] = $this->startTime->copy();
+    }
+
+    public function add30MinutesToDevelopers($developerId): void
+    {
+        $this->currentTimeForDeveloper[$developerId] = $time->copy()->addMinutes(30);
+    }
+
+    function createEventsForTickets($ticketList, $developerId, $status = null): void
+    {
+        foreach ($ticketList as  $ticket) {
+            $this->createEvent($developerId, $ticket, $status);
         }
     }
 
-    public function add30MinutesToAllDevelopers(): void{
-        foreach($this->currentTimeForDeveloper as $developerId => $time){
-            $this->currentTimeForDeveloper[$developerId] = $time->copy()->addMinutes(30);
-        }
+
+    public function getTestedTickets($developerId = null)
+    {
+        $query = Story::where('status', StoryStatus::Tested->value)
+            ->where(function ($query) use ($developerId) {
+                $query->where('creator_id', $developerId)
+                    ->orWhere(function ($query) use ($developerId) {
+                        $query->whereIn('creator_id', function ($subQuery) {
+                            $subQuery->select('id')->from('users')->whereJsonContains('roles', 'customer');
+                        })->where('user_id', $developerId);
+                    });
+            });
+
+        return $query->get();
     }
 
-    function createEventsForTickets($ticketList): void{
-        foreach ($ticketList as $developerId => $tickets) {
-            $currentTimeForTicket = $this->currentTimeForDeveloper[$developerId]->copy();
-            foreach($tickets as $ticket){
-                if($ticket->status == StoryStatus::Test->value && $ticket->tester_id == null){ // Se il ticket non ha un tester, lo assegna allo sviluppatore assegnato
-                    $developerId = $ticket->user_id;
-                    $currentTimeForTicket = $this->currentTimeForDeveloper[$developerId]->copy();
-                }
-                $currentTimeForTicket = $this->createEvent($developerId, $ticket, $currentTimeForTicket);
-                $this->currentTimeForDeveloper[$developerId] = $currentTimeForTicket;
-            }
-        }
-    }
+    private function createEvent($developerId, $ticket, $status = null)
+    {
+        $startTime = $this->currentTimeForDeveloper[$developerId]->copy();
 
-    private function createEvent($developerId, $ticket, Carbon $startTime){
         $developer = DB::table('users')->where('id', $developerId)->first();
 
-            $endTime = $startTime->copy()->addMinutes(30);
+        $endTime = $startTime->copy()->addMinutes(30);
 
-            $colorId = $this->getColorId($ticket);
+        $colorId = $this->getColorId($ticket, $status);
 
-            // Crea un singolo evento per la storia
-            $creator = DB::table('users')->where('id', $ticket->creator_id)->first();
-            try {
-                Event::create([
-                    'name' => "OC: {$ticket->id} [{$creator->name}] - {$ticket->name}", // Nome della storia come titolo dell'evento,
-                    'description' => "{$ticket->description}\n\nType: {$ticket->type}, Status: {$ticket->status}\nLink: https://orchestrator.maphub.it/resources/developer-stories/{$ticket->id}",
-                    'startDateTime' => $startTime,
-                    'endDateTime' => $endTime,
-                    'colorId' => $colorId, // Imposta il colore dell'evento
-                ], $developer->email);
-                $this->info("Event for OC: {$ticket->id} synced to Google Calendar for developer: {$developer->name}");
-            } catch (\Exception $e) {
-                $this->error("Failed to create event for OC: {$ticket->id}. Error: " . $e->getMessage());
-            }
-
-            return $endTime;
+        // Crea un singolo evento per la storia
+        $creator = DB::table('users')->where('id', $ticket->creator_id)->first();
+        try {
+            Event::create([
+                'name' => "OC: {$ticket->id} [{$creator->name}] - {$ticket->name}", // Nome della storia come titolo dell'evento,
+                'description' => "{$ticket->description}\n\nType: {$ticket->type}, Status: {$ticket->status}\nLink: https://orchestrator.maphub.it/resources/developer-stories/{$ticket->id}",
+                'startDateTime' => $startTime,
+                'endDateTime' => $endTime,
+                'colorId' => $colorId, // Imposta il colore dell'evento
+            ], $developer->email);
+            $this->info("Event for OC: {$ticket->id} synced to Google Calendar for developer: {$developer->name}");
+            $this->currentTimeForDeveloper[$developerId] = $endTime;
+        } catch (\Exception $e) {
+            $this->error("Failed to create event for OC: {$ticket->id}. Error: " . $e->getMessage());
+        }
+    }
+    private function createEventLabel($developerId, $label, Carbon $startTime)
+    {
+        $developer = DB::table('users')->where('id', $developerId)->first();
+        $endTime = $startTime->copy()->addMinutes(30);
+        try {
+            Event::create([
+                'name' => "OC: {$label}", // Nome della storia come titolo dell'evento,
+                'startDateTime' => $startTime,
+                'endDateTime' => $endTime,
+                'colorId' => self::DEFAULT_COLOR_ID // Imposta il colore dell'evento
+            ], $developer->email);
+            $this->currentTimeForDeveloper[$developerId] = $endTime;
+        } catch (\Exception $e) {
+        }
     }
 
-    private function deleteCalendar($developerId){
+    private function deleteCalendar($developerId)
+    {
         $developer = DB::table('users')->where('id', $developerId)->first();
-        if($developer && $developer->email){
+        if ($developer && $developer->email) {
             $calendarId = $developer->email;
             $this->deletePreviousEvents($calendarId);
-        }else {
+        } else {
             $this->warn("Developer ID: {$developerId} does not have a valid email.");
         }
     }
 
-    public function getTicketsWithStatus($status, $developerId = null){
+    public function getTicketsWithStatus($status, $developerId = null)
+    {
         // Query per ottenere i ticket con lo status passato come parametro
         $query = Story::where(function ($query) use ($status) {
             $query->whereIn('status', [$status]);
@@ -169,12 +183,12 @@ class SyncStoriesWithGoogleCalendar extends Command
         })
             ->whereNotNull('user_id')
             ->whereNotNull('type');
-            
+
         // Se è stato passato un developerId, filtra i ticket in base a quello
-        if(isset($developerId)){
-            if($status == StoryStatus::Test->value){
+        if (isset($developerId)) {
+            if ($status == StoryStatus::Test->value) {
                 $query = $query->where("tester_id", $developerId);
-            }else{
+            } else {
                 $query = $query->where("user_id", $developerId);
             }
         }
@@ -182,10 +196,14 @@ class SyncStoriesWithGoogleCalendar extends Command
         return $query->get();
     }
 
-    private function getColorId($story){
+    private function getColorId($story, $status = null)
+    {
+        if (is_null($status)) {
+            $status = $story->status;
+        }
         // Imposta il colore dell'evento in base al tipo di storia
         $colorId = self::DEFAULT_COLOR_ID; // Default color (Yellow)
-        if($story->status == StoryStatus::Todo->value){
+        if ($status == StoryStatus::Todo->value) {
             switch ($story->type) {
                 case StoryType::Bug->value:
                     $colorId = self::BUG_COLOR_ID; // Bold Red
@@ -197,8 +215,8 @@ class SyncStoriesWithGoogleCalendar extends Command
                     $colorId = self::FEATURE_COLOR_ID; // Blue
                     break;
             }
-        }else{
-            switch ($story->status) {
+        } else {
+            switch ($status) {
                 case StoryStatus::Test->value:
                     $colorId = self::TESTING_COLOR_ID; // Tangerine
                     break;
