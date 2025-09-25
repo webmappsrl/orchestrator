13a81df8013d894c7775ad40e9e2e3f975e02c53
Alessio Piccioli <piccioli@netseven.it>
2025-09-25 06:32:35 +0200

    feat: Add CAI customer users to initialization script
    - Add 35 CAI customer users (GR regionali, commissioni, sezioni)
    - Update config/initialization.php with complete CAI customer list
    - All customers have password: customer123
    - Total users now: 46 (1 admin, 10 developers, 35 customers)
    - Includes GR regionali, CNSASA, CSC, CCST, CCAG, CCTAM, CCE, CCM, CSMT, SVI, SOBN, SOROA, SOSEC, CNC, SOB-BOSSEA, CAI CULTURA, SODAS, CAI SCUOLA

--- config/initialization.php
+++ config/initialization.php
@@ -15,12 +15,12 @@ return [
         [
             'name' => 'Alessio Piccioli',
             'email' => 'alessio.piccioli@montagnaservizi.com',
-            'roles' => ['admin', 'developer', 'manager', 'editor']
+            'roles' => ['developer']
         ],
         [
             'name' => 'Riccardo Bernasconi',
             'email' => 'riccardo.bernasconi@montagnaservizi.com',
-            'roles' => ['developer', 'manager']
+            'roles' => ['developer']
         ],
         [
             'name' => 'Mattia Bianchi',
@@ -65,12 +65,146 @@ return [
     ],
 
     'customers' => [
-        // Add your customer users here
-        // Example:
-        // [
-        //     'name' => 'Customer Name',
-        //     'email' => 'customer@example.com'
-        // ],
+        [
+            'name' => 'GR Sicilia',
+            'email' => 'gr_cai_sicilia@cai.it'
+        ],
+        [
+            'name' => 'GR Toscana',
+            'email' => 'gr_cai_toscana@cai.it'
+        ],
+        [
+            'name' => 'GR Umbria',
+            'email' => 'gr_cai_umbria@cai.it'
+        ],
+        [
+            'name' => 'GR Val d\'Aosta',
+            'email' => 'gr_cai_valledaosta@cai.it'
+        ],
+        [
+            'name' => 'GR Veneto',
+            'email' => 'gr_cai_veneto@cai.it'
+        ],
+        [
+            'name' => 'GR Marche',
+            'email' => 'gr_cai_marche@cai.it'
+        ],
+        [
+            'name' => 'GR Molise',
+            'email' => 'gr_cai_molise@cai.it'
+        ],
+        [
+            'name' => 'GR Piemonte',
+            'email' => 'gr_cai_piemonte@cai.it'
+        ],
+        [
+            'name' => 'GR Puglia',
+            'email' => 'gr_cai_puglia@cai.it'
+        ],
+        [
+            'name' => 'GR Abruzzo',
+            'email' => 'gr_cai_abruzzo@cai.it'
+        ],
+        [
+            'name' => 'GR Alto Adige',
+            'email' => 'presidente@caialtoadige.it'
+        ],
+        [
+            'name' => 'GR Basilicata',
+            'email' => 'gr_cai_basilicata@cai.it'
+        ],
+        [
+            'name' => 'GR Lazio',
+            'email' => 'gr_cai_lazio@cai.it'
+        ],
+        [
+            'name' => 'GR Liguria',
+            'email' => 'gr_cai_liguria@cai.it'
+        ],
+        [
+            'name' => 'GR Lombardia',
+            'email' => 'gr_cai_lombardia@cai.it'
+        ],
+        [
+            'name' => 'GR Campania',
+            'email' => 'gr_cai_campania@cai.it'
+        ],
+        [
+            'name' => 'GR Emilia',
+            'email' => 'gr_cai_emiliaromagna@cai.it'
+        ],
+        [
+            'name' => 'GR Friuli Venezia Giulia',
+            'email' => 'presidente@cai-fvg.it'
+        ],
+        [
+            'name' => 'CSC',
+            'email' => 'csc@cai.it'
+        ],
+        [
+            'name' => 'CNSASA',
+            'email' => 'cnsasa@cai.it'
+        ],
+        [
+            'name' => 'CCST',
+            'email' => 'ccst@cai.it'
+        ],
+        [
+            'name' => 'CCAG',
+            'email' => 'ccag@cai.it'
+        ],
+        [
+            'name' => 'CCTAM',
+            'email' => 'cctam@cai.it'
+        ],
+        [
+            'name' => 'CCE',
+            'email' => 'cce@cai.it'
+        ],
+        [
+            'name' => 'CCM',
+            'email' => 'ccm@cai.it'
+        ],
+        [
+            'name' => 'CSMT',
+            'email' => 'csmt@cai.it'
+        ],
+        [
+            'name' => 'SVI',
+            'email' => 'svi@cai.it'
+        ],
+        [
+            'name' => 'Biblioteca Nazionale',
+            'email' => 'sobn@cai.it'
+        ],
+        [
+            'name' => 'SOROA',
+            'email' => 'soroa@cai.it'
+        ],
+        [
+            'name' => 'SOSEC',
+            'email' => 'sosec@cai.it'
+        ],
+        [
+            'name' => 'CNC',
+            'email' => 'cnc@cai.it'
+        ],
+        [
+            'name' => 'SOB - BOSSEA',
+            'email' => 'bossea@cai.it'
+        ],
+        [
+            'name' => 'CAI CULTURA',
+            'email' => 'a.massena@cai.it'
+        ],
+        [
+            'name' => 'SODAS',
+            'email' => 'sodas@cai.it'
+        ],
+        [
+            'name' => 'CAI SCUOLA',
+            'email' => 'caiscuola@cai.it'
+        ],
     ],
 
     'tags' => [
