1715fc21cfddb1ae29963e5b2b0ad6691a7e4f3b
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2026-02-02 12:29:05 +0100

    fix: correct asset path and improve number formatting oc: 7045
    Updates script path for renewal formatting, consolidating it
    into a new location for better organization.
    
    Replaces meta modification approach with a consistent
    Italian number formatting function on the renewals page.
    
    Relates to oc_7045

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -48,7 +48,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
         Nova::style('nova-custom', public_path('/nova-custom.css'));
 
-        Nova::script('renewals-italian-format', public_path('nova-renewals-format.js'));
+        Nova::script('renewals-italian-format', public_path('js/renewals-format.js'));
 
         Nova::mainMenu(function (Request $request) {
             $newStoryUrl = '/resources/stories/new';
@@ -62,7 +62,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
                 MenuItem::externalLink('SCRUM', route('scrum.meeting', ['meetCode' => $scrumMeetCode]))->openInNewTab()->canSee(function ($request) {
                     return $request->user()->hasRole(UserRole::Admin) || $request->user()->hasRole(UserRole::Manager) || $request->user()->hasRole(UserRole::Developer);
                 }),
-                MenuItem::externalLink('MEET', 'https://meet.google.com/'.$scrumMeetCode)->openInNewTab()->canSee(function ($request) {
+                MenuItem::externalLink('MEET', 'https://meet.google.com/' . $scrumMeetCode)->openInNewTab()->canSee(function ($request) {
                     return $request->user()->hasRole(UserRole::Admin) || $request->user()->hasRole(UserRole::Manager) || $request->user()->hasRole(UserRole::Developer);
                 }),
                 MenuSection::dashboard(Kanban::class)->icon('chart-bar')->canSee(function ($request) {
@@ -249,7 +249,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
                 // Per tutti gli altri utenti (Admin, Manager, Developer),
                 // reindirizza alla dashboard Kanban
-                if ($user->hasRole(UserRole::Admin) ||
+                if (
+                    $user->hasRole(UserRole::Admin) ||
                     $user->hasRole(UserRole::Manager) ||
                     $user->hasRole(UserRole::Developer)
                 ) {
